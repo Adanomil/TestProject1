9665509f9e7796ddb03e49623f0954dd9181bebc
Jonathan Knowles <jonathan@knowlesfamily.com>
2012-01-23 18:23:18 -0800

    Test Project.

---
+++ src/testproject1/TestProject1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testproject1;
+
+/**
+ *
+ * @author workshopjan23
+ */
+public class TestProject1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
